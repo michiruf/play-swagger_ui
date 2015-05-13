d68a1c6a9c75a735e08609f6e17d5a1d82ad0915
Michael Ruf <michael.ruf@socialbit.de>
2015-05-13 17:22:53 +0200

    Initial project share

---
+++ app/controllers/swagger_ui/SwaggerUIController.java
@@ -0,0 +1,25 @@
+package controllers.swagger_ui;
+
+import play.Play;
+import play.mvc.Controller;
+
+/**
+ * @author Michael Ruf
+ * @since 2015-05-05
+ */
+@SuppressWarnings("unused")
+public class SwaggerUIController extends Controller {
+
+    public static void index() {
+        render("swagger_ui/index.html");
+    }
+
+    public static void o2c() {
+        render("swagger_ui/o2c.html");
+
+//        #{if(Play.configuration.containsKey("swagger_ui.baseUrl")}
+//            Play.configuration.getProperty()
+//        #
+    }
+
+}
